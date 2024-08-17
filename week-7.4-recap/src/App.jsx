4ba76bf3772d23da0d851364842dcf26a308b794
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-17 23:57:37 +0530

    Computation memoized

--- week-7.4-recap/src/App.jsx
+++ week-7.4-recap/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
-import { jobsAtom, messagingAtom, networkAtom,  notificationsAtom } from './atoms'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom } from './atoms'
+import { useMemo } from 'react'
 
 function App() {
 
@@ -16,11 +17,12 @@ function App() {
 function MainApp() {
 
   const networkNotificationsCount = useRecoilValue(networkAtom)
-  const jobsCount =useRecoilValue(jobsAtom)
-  const notifications=useRecoilValue(notificationsAtom)
-  const[messages,setMessages]=useRecoilState(messagingAtom)
-//useMemo
-  const totalNotifications=networkNotificationsCount+messages+notifications+jobsCount;
+  const jobsCount = useRecoilValue(jobsAtom)
+  const notifications = useRecoilValue(notificationsAtom)
+  const [messages, setMessages] = useRecoilState(messagingAtom)
+  //useMemo
+  const totalNotifications = useMemo(() =>  (networkNotificationsCount + messages + notifications + jobsCount) ,
+    [messages, notifications, jobsCount, networkNotificationsCount])
 
 
   return <div>
@@ -29,7 +31,7 @@ function MainApp() {
     <button>Jobs({jobsCount})</button>
     <button>Messaging({messages})</button>
     <button>Notifications({notifications})</button>
-    <button onClick={()=>setMessages(messages+1)}>Me{totalNotifications}</button>
+    <button onClick={() => setMessages(messages + 1)}>Me{totalNotifications}</button>
   </div>
 }
 
