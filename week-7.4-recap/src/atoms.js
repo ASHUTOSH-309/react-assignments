fbd93c17054e48e6abc6689f34d2801d4bf7600b
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-17 22:53:18 +0530

    Added four atoms to emulate linkedln footerstate

---
+++ week-7.4-recap/src/atoms.js
@@ -0,0 +1,23 @@
+import { atom } from "recoil";
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 104
+
+
+})
+export const jobsAtom = atom({
+
+    key: "jobsAtom",
+    default: 20
+
+})
+
+export const notificationsAtom = atom({
+    key: "notificationsAtom",
+    default: 30
+})
+export const messagingAtom = atom({
+    key: "messagingAtom",
+    default: 44
+})
