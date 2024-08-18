26212b5af2bb618525906fbb1a18db3f4bbffad2
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-18 15:13:39 +0530

    Added Delay of 5 seconds

--- week-7.4-recap/src/atoms.js
+++ week-7.4-recap/src/atoms.js
@@ -6,6 +6,8 @@ export const notifications = atom({
     default: selector({
         key: "networkAtomSelector",
         get: async () => {
+            //Nothing renders till the request resolves, this can be proved by adding an artificial delay of 5 sec, which will render a blank screen"
+            await new Promise(r=>setTimeout(r,5000))
             const res = await axios.get("https://sum-server.100xdevs.com/notifications")
             return res.data
         }
